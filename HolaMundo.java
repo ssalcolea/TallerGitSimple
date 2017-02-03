7adc6313c7146fda9edfaadd205b0789ddae89d9
Pepito <pepito@gfi.es>
2017-02-03 12:21:52 +0100

    Se incluye quien dice el hola mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,7 @@ public class HolaMundo {
 
   public static void main(String[] args) {
     // Prints Hello World
-    System.out.println ("Hola mundo");
+    System.out.println ("Pepito: Hola mundo");
   }
 
 }
