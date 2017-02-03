385af88893f9c8c9fff2c3875454002e924edfef
Sergio Sanchez Alcolea <ssalcolea@gfi.es>
2017-02-03 12:53:22 +0100

    Contestacion

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,8 @@ public class HolaMundo {
 
   public static void main(String[] args) {
     // Prints Hello World
-    System.out.println ("Hola mundo");
+    System.out.println ("Pepito: Hola mundo");
+    System.out.println ("Juanito: Hola mundo");
   }
 
 }
