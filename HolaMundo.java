9b081c07f8668d05602bfdf83f6d7064dafc457e
Sergio Sanchez Alcolea <ssalcolea@gfi.es>
2017-02-03 12:28:46 +0100

    Se pone admiracion

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,7 @@ public class HolaMundo {
 
   public static void main(String[] args) {
     // Prints Hello World
-    System.out.println ("Pepito: Hola mundo");
+    System.out.println ("Pepito: Hola mundo!!!");
   }
 
 }
